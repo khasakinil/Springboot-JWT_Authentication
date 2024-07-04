d2f0fb1182892ad177c18579db8a3793a6df7a29
khasakinil <khasakinil@gmail.com>
2024-07-04 08:34:11 +0530

    jwt implementation initial commit

---
+++ Jwt-Impl/src/main/java/com/nk/Jwt_Impl/JwtImplApplication.java
@@ -0,0 +1,13 @@
+package com.nk.Jwt_Impl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtImplApplication.class, args);
+	}
+
+}
---
+++ Jwt-Impl/src/test/java/com/nk/Jwt_Impl/JwtImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nk.Jwt_Impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
